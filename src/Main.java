eeb8297214dbd1f1ec3710d9482bf0a293a03143
siemens <sema.perstnev.01@mail.ru>
2023-02-10 02:40:54 +0300

    Задача, билет №6, Перстнев С.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = {42,76,34,23,35,87,54,12,98,93,85,65,11,14};
+        System.out.println(Arrays.toString(getArray(array)));
+    }
+
+    public static int[] getArray(int[] array) {
+        int[] array1 = new int[5];
+        for (int i = 0, j = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                array[i]++;
+            }
+            if (i > 1 && i < 7) {
+                array1[j] = array[i];
+                j++;
+            }
+        }
+        return array1;
     }
 }
